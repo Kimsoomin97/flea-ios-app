23171cfd482a608f5baad4f01718bf56db88475b
Soomin <>
2022-04-12 15:59:36 +0900

    FEAT : My Page UI 구현

--- Flea_Market/Controller/MainHomeViewController.swift
+++ Flea_Market/Controller/MainHomeViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class MainHomeViewController: UIViewController {
     
+//    private let memberViewLabel: UILabel = {
+//        let label = UILabel()
+//        label.text = "Flae Market"
+//        label.font = .systemFont(ofSize: 50, weight: .bold)
+//        return label
+//    }()
+    
     private let registButton: UIButton = {
         let button = UIButton()
 //        button.setTitle("회원가입 폼 이동", for: .normal)
@@ -24,6 +31,15 @@ class MainHomeViewController: UIViewController {
 //        button.setTitleColor(.label, for: .normal)
         button.addTarget(self, action: #selector(didTapLoginButton), for: .touchUpInside)
         button.setBackgroundImage(UIImage(named: "login"), for: .normal)
+        return button
+    }()
+
+    private let MypageButton: UIButton = {
+        let button = UIButton()
+//        button.setTitle("Mypage 폼 이동", for: .normal)
+//        button.setTitleColor(.label, for: .normal)
+        button.addTarget(self, action: #selector(didTapMypageButton), for: .touchUpInside)
+        button.setBackgroundImage(UIImage(named: "Mypage"), for: .normal)
         return button
     }()
     
@@ -64,9 +80,21 @@ class MainHomeViewController: UIViewController {
         self.navigationController?.pushViewController(vc, animated: true)
         
     }
+
+    @objc func didTapMypageButton() {
+        let vc = myPageViewController()
+//        self.present(vc, animated: true, completion: nil)
+        self.navigationController?.pushViewController(vc, animated: true)
+        
+    }
     
     private func configureUI() {
         view.backgroundColor = .systemBackground
+        
+//        view.addSubview(memberViewLabel)
+//        memberViewLabel.translatesAutoresizingMaskIntoConstraints = false
+//        memberViewLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+//        memberViewLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 60).isActive = true
         
         view.addSubview(registButton)
         registButton.translatesAutoresizingMaskIntoConstraints = false
@@ -83,6 +111,14 @@ class MainHomeViewController: UIViewController {
         loginButton.widthAnchor.constraint(equalToConstant: 80).isActive = true
         loginButton.heightAnchor.constraint(equalToConstant: 80).isActive = true
         loginButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -70).isActive = true
+        
+        view.addSubview(MypageButton)
+        MypageButton.translatesAutoresizingMaskIntoConstraints = false
+        MypageButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        MypageButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        MypageButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        MypageButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        MypageButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -70).isActive = true
         
 //        view.addSubview(imageView)
 //        imageView.translatesAutoresizingMaskIntoConstraints = false
--- Flea_Market/Controller/myPageViewController.swift
+++ Flea_Market/Controller/myPageViewController.swift
@@ -130,67 +130,68 @@ class myPageViewController: UIViewController {
         
         view.addSubview(idLabel)
         idLabel.translatesAutoresizingMaskIntoConstraints = false
-        idLabel.topAnchor.constraint(equalTo: myPageViewLabel.bottomAnchor, constant: 60).isActive = true
+        idLabel.topAnchor.constraint(equalTo: myPageViewLabel.topAnchor, constant: 80).isActive = true
+//        idLabel.topAnchor.constraint(equalTo: myPageViewLabel.bottomAnchor, constant: -160).isActive = true
         idLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
         
         view.addSubview(idViewLabel)
         idViewLabel.translatesAutoresizingMaskIntoConstraints = false
-        idViewLabel.topAnchor.constraint(equalTo: registrationLabel.bottomAnchor, constant: 60).isActive = true
+        idViewLabel.topAnchor.constraint(equalTo: myPageViewLabel.topAnchor, constant: 80).isActive = true
         idViewLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 150).isActive = true
         
         view.addSubview(pwLabel)
         pwLabel.translatesAutoresizingMaskIntoConstraints = false
-        pwLabel.topAnchor.constraint(equalTo: idLabel.bottomAnchor, constant: 30).isActive = true
+        pwLabel.topAnchor.constraint(equalTo: idLabel.topAnchor, constant: 50).isActive = true
         pwLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
         
         view.addSubview(pwTextField)
         pwTextField.translatesAutoresizingMaskIntoConstraints = false
-        pwTextField.topAnchor.constraint(equalTo: idViewLabel.bottomAnchor, constant: 30).isActive = true
+        pwTextField.topAnchor.constraint(equalTo: idViewLabel.topAnchor, constant: 50).isActive = true
         pwTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 150).isActive = true
         
         view.addSubview(confiPWTextField)
         confiPWTextField.translatesAutoresizingMaskIntoConstraints = false
-        confiPWTextField.topAnchor.constraint(equalTo: pwTextField.bottomAnchor, constant: 10).isActive = true
+        confiPWTextField.topAnchor.constraint(equalTo: pwTextField.topAnchor, constant: 20).isActive = true
         confiPWTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 150).isActive = true
         
         view.addSubview(emailLabel)
         emailLabel.translatesAutoresizingMaskIntoConstraints = false
-        emailLabel.topAnchor.constraint(equalTo: confiPWTextField.bottomAnchor, constant: 30).isActive = true
+        emailLabel.topAnchor.constraint(equalTo: confiPWTextField.topAnchor, constant: 40).isActive = true
         emailLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
         
         view.addSubview(emailTextField)
         emailTextField.translatesAutoresizingMaskIntoConstraints = false
-        emailTextField.topAnchor.constraint(equalTo: confiPWTextField.bottomAnchor, constant: 30).isActive = true
+        emailTextField.topAnchor.constraint(equalTo: confiPWTextField.topAnchor, constant: 30).isActive = true
         emailTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 150).isActive = true
         
         view.addSubview(phoneLabel)
         phoneLabel.translatesAutoresizingMaskIntoConstraints = false
-        phoneLabel.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: 30).isActive = true
+        phoneLabel.topAnchor.constraint(equalTo: emailLabel.topAnchor, constant: 30).isActive = true
         phoneLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
         
         view.addSubview(phoneTextField)
         phoneTextField.translatesAutoresizingMaskIntoConstraints = false
-        phoneTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 30).isActive = true
+        phoneTextField.topAnchor.constraint(equalTo: emailTextField.topAnchor, constant: 30).isActive = true
         phoneTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 150).isActive = true
         
         view.addSubview(nicknameLabel)
         nicknameLabel.translatesAutoresizingMaskIntoConstraints = false
-        nicknameLabel.topAnchor.constraint(equalTo: phoneLabel.bottomAnchor, constant: 30).isActive = true
+        nicknameLabel.topAnchor.constraint(equalTo: phoneLabel.topAnchor, constant: 30).isActive = true
         nicknameLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
         
         view.addSubview(nicknameTextField)
         nicknameTextField.translatesAutoresizingMaskIntoConstraints = false
-        nicknameTextField.topAnchor.constraint(equalTo: phoneTextField.bottomAnchor, constant: 30).isActive = true
+        nicknameTextField.topAnchor.constraint(equalTo: phoneTextField.topAnchor, constant: 30).isActive = true
         nicknameTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 150).isActive = true
         
         view.addSubview(addressLabel)
         addressLabel.translatesAutoresizingMaskIntoConstraints = false
-        addressLabel.topAnchor.constraint(equalTo: nicknameLabel.bottomAnchor, constant: 30).isActive = true
+        addressLabel.topAnchor.constraint(equalTo: nicknameLabel.topAnchor, constant: 30).isActive = true
         addressLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
         
         view.addSubview(addressTextField)
         addressTextField.translatesAutoresizingMaskIntoConstraints = false
-        addressTextField.topAnchor.constraint(equalTo: nicknameTextField.bottomAnchor, constant: 30).isActive = true
+        addressTextField.topAnchor.constraint(equalTo: nicknameTextField.topAnchor, constant: 30).isActive = true
         addressTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 150).isActive = true
         
         
